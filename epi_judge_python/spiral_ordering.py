48618de6365e0f1f4c225cf394aff9479d377343
Cole Thiessen <colekthiessen@gmail.com>
2021-05-28 08:51:56 -0600

    string integer interconversion

--- epi_judge_python/spiral_ordering.py
+++ epi_judge_python/spiral_ordering.py
@@ -2,10 +2,46 @@ from typing import List
 
 from test_framework import generic_test
 
-
 def matrix_in_spiral_order(square_matrix: List[List[int]]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    L, R = 0, len(square_matrix)
+    T, B = 0, len(square_matrix)
+
+    res = []
+    direction = 0
+    while (L <= R and T <= B):
+        if direction == 0:
+            for i in range(L, R):
+                res.append(square_matrix[T][i])
+            T += 1
+        elif direction == 1:
+            for i in range(T, B):
+                res.append(square_matrix[i][R - 1])
+            R -= 1
+        elif direction == 2:
+            for i in range(R- 1, L - 1, -1):
+                res.append(square_matrix[B - 1][i])
+            B -= 1
+        elif direction == 3:
+            for i in range(B - 1, T - 1, -1):
+                res.append(square_matrix[i][L])
+            L += 1
+        direction = (direction + 1) % 4
+
+    return res
+
+# def matrix_in_spiral_order(square_matrix: List[List[int]]) -> List[int]:
+#     res = []
+#     def process_layer(offset):
+#         if offset == len(square_matrix) - offset - 1:
+#             res.append(square_matrix[offset][offset])
+#             return
+
+#         res.extend(square_matrix[offset][-1 - offset])
+#         res.extend(list(zip(*square_matrix)))[-1 - offset][offset:-1 - offset]
+#         res.extend(square_matrix[-1 - offset][-1 - offset: offset: -1])
+#         res.extend(list(zip(*square_matrix)))[-1 - offset][offset:-1 - offset]
+
+        
 
 
 if __name__ == '__main__':
--- epi_judge_python/string_integer_interconversion.py
+++ epi_judge_python/string_integer_interconversion.py
@@ -1,15 +1,49 @@
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
+import functools
+import string
+
 
 def int_to_string(x: int) -> str:
-    # TODO - you fill in here.
-    return '0'
+    itos = []
+
+    neg = False
+    if(x < 0):
+        neg = True
+        x = abs(x)
+
+    while True:
+        next_int = x % 10
+        itos.append(chr(ord('0') + next_int))
+        x = x // 10
+        if x == 0:
+            break
+
+    return ('-' if neg else '') + ''.join(reversed(itos))
+
 
 
 def string_to_int(s: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    neg = False
+    start = 0
+
+    if(s[0] == '-'):
+        neg = True
+        start = 1
+    
+    if(s[0] == '+'):
+        start = 1
+
+    res = 0
+    for i in range(start, len(s)):
+        res += ord(s[i]) - ord('0')
+        res *= 10 if i < len(s) - 1 else 1
+
+    return (-res if neg else res)
+
+# def string_to_int(s: str) -> int:
+#     return functools.reduce(lambda running_sum, c: running_sum * 10 + string.digits.index(c), s[s[0] == '-':], 0) * (-1 if s[0] == '-' else 1)
 
 
 def wrapper(x, s):
@@ -20,6 +54,7 @@ def wrapper(x, s):
 
 
 if __name__ == '__main__':
+    # print(string_to_int('4176473'))
     exit(
         generic_test.generic_test_main('string_integer_interconversion.py',
                                        'string_integer_interconversion.tsv',
