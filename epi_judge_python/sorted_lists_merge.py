321f55c7be19da4ab1dc9f1013f004fc83ecca32
Cole Thiessen <colekthiessen@gmail.com>
2021-05-31 19:30:47 -0600

    Merge 2 sorted linked lists

--- epi_judge_python/sorted_lists_merge.py
+++ epi_judge_python/sorted_lists_merge.py
@@ -3,11 +3,24 @@ from typing import Optional
 from list_node import ListNode
 from test_framework import generic_test
 
-
 def merge_two_sorted_lists(L1: Optional[ListNode],
                            L2: Optional[ListNode]) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    
+    # 2 iterators
+    # both start at head
+    # when one ends, make sure to fill rest of other list
+    sentinel = res = ListNode()
+
+    while L1 and L2:
+        if L1.data < L2.data:
+            res.next, L1 = L1, L1.next
+        else:
+            res.next, L2 = L2, L2.next
+        res = res.next
+
+    res.next = L1 or L2
+    return sentinel.next
+
 
 
 if __name__ == '__main__':
